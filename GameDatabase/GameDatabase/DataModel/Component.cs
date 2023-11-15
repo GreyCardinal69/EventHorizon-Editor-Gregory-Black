1646faf37b94717a1480580e38e5e3dad702e6d7
The Grey Cardinal <mkctgctg@mail.ru>
2023-11-15 15:31:45 +0400

    Fixed celltype and weaponslottype not being editable.
    Fixed celltype and weaponslottype not being editable.

--- GameDatabase/GameDatabase/DataModel/Component.cs
+++ GameDatabase/GameDatabase/DataModel/Component.cs
@@ -7,14 +7,22 @@ namespace EditorDatabase.DataModel
 {
 	public partial class Component
 	{
-        partial void OnDataDeserialized(ComponentSerializable serializable, Database database)
+        partial void OnDataDeserialized( ComponentSerializable serializable, Database database )
         {
-
+            if ( !string.IsNullOrEmpty( serializable.WeaponSlotType ) )
+                WeaponSlotType = ( WeaponSlotType ) serializable.WeaponSlotType.First();
+            if ( !string.IsNullOrEmpty( serializable.CellType ) )
+                CellType = ( CellType ) serializable.CellType.First();
         }
 
-		partial void OnDataSerialized(ref ComponentSerializable serializable)
+        partial void OnDataSerialized( ref ComponentSerializable serializable )
         {
             serializable.AmmunitionId = Ammunition.IsNull ? AmmunitionObsolete.Value : Ammunition.Value;
+            serializable.WeaponSlotType = ( ( char ) WeaponSlotType ).ToString();
+            serializable.CellType = ( ( char ) CellType ).ToString();
         }
-	}
+
+        public WeaponSlotType WeaponSlotType;
+        public CellType CellType;
+    }
 }
